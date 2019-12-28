4c92a5f22ee982de06ac4bc1329647ba19ec6877
Andrew <root@osboxes>
2019-12-28 14:06:24 -0500

    First java program

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, Wolrd" in the terminal windows.
+             System.out.println("Hello, World");
+   }
+}
